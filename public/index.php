a001df3b5cdf882163c4462a73584e187dcb6c1d
RafaelAmadei <orafaelamadei@gmail.com>
2025-11-19 00:54:08 +0000

    commit

--- public/index.php
+++ public/index.php
@@ -1,17 +1,24 @@
-<?php
-require '../vendor/autoload.php';
-
-use Application\Hello;
-?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <link href="/css/default.css" rel="stylesheet" />
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="./css/default.css" rel="stylesheet" />
 </head>
 <body>
-    <h1>PHP & Mysql</h1>
-    <p><?= Hello::message() ?></p>
+
+<nav class="navbar navbar-expand-lg navbar-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="Captura de tela 2025-11-18 212647.png" alt="" width="32" height="32">
+
+        </a>
+    </div>
+</nav>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+    
 </body>
 </html>
